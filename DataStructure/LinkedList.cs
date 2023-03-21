fe143bb6897e96f97e650d35159d4c748d6ddb61
Vaibhav <vaibhavovhal15@gmail.com>
2023-03-21 09:59:09 +0530

    [Vaibhav] Add:Data Structure

---
+++ DataStructure/LinkedList.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructure
+{
+    public class LinkedList
+    {
+        public Node head;
+        public void Add(int data)
+        {
+            Node node = new Node(data);
+            if(this.head == null)
+            {
+                this.head = node;
+            }
+            else
+            {
+                Node temp = head;
+                while(temp.next != null)
+                {
+                    temp = temp.next;
+                }
+                temp.next = node;
+            }
+            Console.WriteLine("{0} inserted into linked list", node.data);
+        }
+    }
+}
---
+++ DataStructure/Program.cs
@@ -0,0 +1,17 @@
+﻿using DataStructure;
+using System;
+
+namespace DataStructire
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Data Structure");
+            LinkedList linkedList = new LinkedList();
+            linkedList.Add(56);
+            linkedList.Add(30);
+            linkedList.Add(70);            
+        }
+    }
+}
